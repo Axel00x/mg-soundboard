6854cfe40c1477d03a194d59307db14aaf467369
Axel00x <alessandro.zeppa@icloud.com>
2024-09-10 21:40:43 +0200

    Hide support prompt

---
+++ common/run.bat
@@ -0,0 +1,25 @@
+@echo off
+cls
+ 
+title mg-soundboard Launcher
+
+echo ================================================================
+echo                    MG-SOUNDBOARD LAUNCHER
+echo ================================================================
+
+echo.
+echo WARNING: Do not close this launcher window while the application is running.
+echo.
+
+echo To start the mg-soundboard application, Python will be executed.
+echo Please wait...
+
+python main.py
+
+echo.
+echo ================================================================
+echo                    MG-SOUNDBOARD TERMINATED
+echo ================================================================
+echo.
+echo The mg-soundboard application has been closed. You can close this window.
+pause
